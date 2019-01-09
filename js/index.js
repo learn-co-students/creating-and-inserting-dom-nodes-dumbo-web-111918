253b6cd5890220287c3b16229ecb285d1ccafe88
jriv95 <joel.rivera7995@gmail.com>
2019-01-09 16:16:07 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+function created () {
+  const element = document.body.createElement("div");
+  element.innerText("Hey DOM");
+  element.style.backgroundColor("#eeeeee");
+  document.appendChild(element);
+
+  console.log("Done")
+}
+
+created();
